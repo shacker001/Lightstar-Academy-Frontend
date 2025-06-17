cabeac4a16fa08666a42c056926ed819cc54a51c
shacker001 <ojsteve01@gmail.com>
2025-06-17 08:14:23 -0700

    ..

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: './src',  // if your index.html is inside src/
   plugins: [react()],
 })
