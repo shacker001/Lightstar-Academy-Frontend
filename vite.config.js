ad7fb37f9433db40e9f1f26130858219f1ee22f1
shacker001 <ojsteve01@gmail.com>
2025-06-17 07:17:04 -0700

    erro-fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "/Lightstar-Academy-Frontend",
-});
+  plugins: [react()],
+})
