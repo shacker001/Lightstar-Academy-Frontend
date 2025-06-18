206d5d2ea6d834882f8a397317fd5d73a8e0e310
shacker001 <ojsteve01@gmail.com>
2025-06-17 23:06:49 -0700

    path

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.spornsorship-frontend/src',  // if your index.html is inside src/
-  plugins: [react()],
-})
-
-// export default defineConfig({
-//     root: './src',
-//     build: {
-//       rollupOptions: {
-//         input: './src/index.html', // Explicitly defining entry point
-//       }
-//     },
-//     plugins: [react()],
-//   });
+    root: './src',
+    build: {
+      rollupOptions: {
+        input: '../sponsorship/src/index.html', // Adjust path reference
+      }
+    },
+    plugins: [react()],
+  });
   
